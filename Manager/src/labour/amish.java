c57a27db7ccc5b41445c7c95c7e5d78aac5c5fd6
300400552 <300400552@N5107S22.ad.douglascollege.ca>
2025-04-14 12:14:58 -0700

    this is work

---
+++ Manager/src/labour/amish.java
@@ -0,0 +1,11 @@
+package labour;
+
+public class amish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("this is the boss");
+          System.out.println("do something");
+	}
+
+}
